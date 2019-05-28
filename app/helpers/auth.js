a92933e3d1f47d89756080a4febcf56cdfe693da
Ronald Cardona Martinez <ronaldkm98@gmail.com>
2019-05-28 18:05:57 -0500

    Manejo de sesiones OK

--- app/helpers/auth.js
+++ app/helpers/auth.js
@@ -2,13 +2,29 @@
  * This helper lets us check is the user is already authenticated for some functions on the app. 
  */
 const helpers = {};
+var userPool = require('./cognito');
 
-helpers.isAuthenticated = (req, res, next) => {
+/** helpers.isAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
   req.flash('error_msg', 'Not Authorized.');
   res.redirect('/login');
-};
+};*/
+
+helpers.isAuthenticated = (req, res, next) => {
+  var cognitoUser = userPool.getCurrentUser();
+  if (cognitoUser != null) {
+    cognitoUser.getSession(function(err, session) {
+      if(err) {
+        req.flash('error_msg', err.message);
+        res.redirect('/login');
+      } else {
+        return next();
+      }
+      console.log('session validity: ' + session.isValid());
+    });
+  }
+}
 
 module.exports = helpers;
---
+++ app/helpers/cognito.js
@@ -0,0 +1,25 @@
+/**
+ * This helper lets us check is the user is already authenticated for some functions on the app. 
+ */
+const helpers = {};
+
+helpers.isAuthenticated = (req, res, next) => {
+  if (cognitoUser != null) {
+    return next();
+  }
+  req.flash('error_msg', 'Not Authorized.');
+  res.redirect('/login');
+};
+
+const config = require('../config.json');
+const AWS = require('aws-sdk');
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+
+const poolData = {
+  UserPoolId: config.cognito.userPoolId,
+  ClientId: config.cognito.clientId
+};
+
+var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+module.exports = userPool;
