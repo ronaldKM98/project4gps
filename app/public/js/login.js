9740d6105dd2f0df9b62441a5176ad33b1da9a51
Camila <camila@MacBook-Pro-de-Camila.local>
2019-06-01 20:39:25 -0500

    Login con cognito

---
+++ app/public/js/login.js
@@ -0,0 +1,41 @@
+function login() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    var poolData = {
+        UserPoolId: _config.cognito.userPoolId,
+        ClientId: _config.cognito.userPoolClientId
+    };
+
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+    const authenticationData = {
+        Username: email,
+        Password: password
+    }
+
+    const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+
+    const userData = {
+        Username: email,
+        Pool: userPool
+    }
+
+    cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: result => {
+            var accessToken = result.getAccessToken();
+            //console.log("access token " + accessToken + '\n');
+            var idToken = result.getIdToken();
+            //console.log(idToken);
+            var refreshToken = result.getRefreshToken();
+            //console.log("refresh token " + refreshToken + '\n');
+            console.log("HICE LOGIN");
+            window.location.replace("/allRoutes");
+        },
+        onFailure: function(err) {
+            alert(err.message);
+        }
+    });
+}
