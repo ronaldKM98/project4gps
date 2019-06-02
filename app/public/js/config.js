3f967228c1cfec68e708879dd6cd9fb90f2ca86d
Camila <camila@MacBook-Pro-de-Camila.local>
2019-06-01 20:00:46 -0500

    Funciona signup desde cliente

---
+++ app/public/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: '******',
+        userPoolClientId: '******',
+        region: 'us-east-2'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
